d5f221a9b88a99886b841ff39b0d7bfdce0c0eae
Gideon-Gyebi <gyebigideon000@gmail.com>
2023-09-20 22:17:15 +0000

    Test & Readme

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -74,11 +74,11 @@ class test_basemodel(unittest.TestCase):
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
-    """def test_kwargs_one(self):
+    def test_kwargs_none(self):
         """ """
-        n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)"""
+        n = {None: None}
+        with self.assertRaises(TypeError):
+            new = self.value(**n)
 
     def test_id(self):
         """ """
@@ -97,3 +97,7 @@ class test_basemodel(unittest.TestCase):
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertFalse(new.created_at == new.updated_at)"""
+
+
+if __name__ == "__main__":
+    unittest.main()
