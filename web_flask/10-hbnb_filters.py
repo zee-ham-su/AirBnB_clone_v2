74ee13107cfb1d4af47e2776e14928dd22b4dc55
zee-ham-su <hsufiian@yahoo.com>
2023-10-21 02:21:45 -0700

    filters

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -2,6 +2,10 @@
 """a script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
 app = Flask(__name__)
 
 
@@ -14,11 +18,11 @@ def close_storage(excep):
 @app.route('/hbnb_filters', strict_slashes=False)
 def filters_hbnb():
     """display hbnb filter html page"""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
     return (render_template("10-hbnb_filters.html",
                             states=states, amenities=amenities))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
